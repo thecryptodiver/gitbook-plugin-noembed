a8e9899c83fec0e14eb94050a6ffb1a3089f7019
thecryptodiver <thecryptodiver@protonmail.com>
2022-01-16 23:39:11 +0530

    update index

--- index.js
+++ index.js
@@ -6,17 +6,25 @@ async function noembed(url) {
 
   if (!!url.length) {
     endpoint += 'url=' + encodeURIComponent(url);
-    try {
-      const data = await axios.get(endpoint);
-      console.log("file: index.js ~ line 10 ~ noembed ~ data", data);
-      if (data) {
-        return data?.html || "Hello, not here";
-      }
-      return "Hello URL";
-    } catch (e) {
-      console.log("file: index.js ~ line 17 ~ noembed ~ e", e);
-      return "Hello error";
-    }
+    return axios.get(endpoint)
+      .then(function (response) {
+        console.log(JSON.stringify(response));
+        return "Hello URL;"
+      })
+      .catch(function (error) {
+        console.log(JSON.stringify(error));
+        return "Hello error;"
+      })
+      .then(function () {
+        console.log("Here123");
+        return "Hello end;"
+      });  
+      // const data = await axios.get(endpoint);
+      // console.log("file: index.js ~ line 10 ~ noembed ~ data", data);
+      // if (data) {
+      //   return data?.html || "Hello, not here";
+      // }
+      // return "Hello URL";
   }
 
   return url;
